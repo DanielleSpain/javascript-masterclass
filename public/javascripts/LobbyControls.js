6034b9919281b0fe16fa85572ae124f8f3fb1cad
Danielle Spain <daniellespain42@gmail.com>
2015-05-19 18:41:19 +1200

    gh

---
+++ public/javascripts/LobbyControls.js
@@ -0,0 +1,20 @@
+
+var LobbyControls = React.createClass({
+  render: function() {
+    var lobbyClass = "";
+
+    if (!this.props.showLobby) {
+      lobbyClass = "hidden";
+    }
+
+    return (
+      <div className={lobbyClass}>
+        <h2>You are not in a room</h2>
+        <p>Enter an existing room:</p>
+        <JoinExisting />
+        <p>Or create a new one:</p>
+        <CreateNew />
+      </div>
+    );
+  }
+});
